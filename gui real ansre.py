e12b31f5b625e8c1eedf59b42ffb56db4de006f4
DanishGent <gust3343@educampusvejle.dk>
2023-04-24 20:58:10 +0200

    fhg

--- gui real ansre.py
+++ gui real ansre.py
@@ -50,9 +50,11 @@ style.theme_use('default')
 style.configure("Treeview", background=treeview_background, foreground=treeview_foreground, rowheight=rowheight, fieldbackground=treeview_background)
 style.map('Treeview', background=[('selected', treeview_selected)])
 
-tree_1_scrollbar = tk.Scrollbar(main_window)
+frame_1 = tk.LabelFrame(main_window, text="Container")
+
+tree_1_scrollbar = tk.Scrollbar(frame_1)
 tree_1_scrollbar.grid(row=0, column=0, padx=padx, pady=pady, sticky='ns')
-tree_1 = ttk.Treeview(main_window, yscrollcommand=tree_1_scrollbar.set, selectmode="browse")
+tree_1 = ttk.Treeview(frame_1, yscrollcommand=tree_1_scrollbar.set, selectmode="browse")
 tree_1.grid(row=0, column=0, padx=0, pady=pady)
 tree_1_scrollbar.config(command=tree_1.yview)
 
@@ -73,56 +75,56 @@ tree_1.tag_configure('evenrow', background=evenrow)
 
 tree_1.bind("<ButtonRelease-1>", lambda event: edit_record(event, tree_1))
 
-frame_1 = tk.LabelFrame(main_window, text="Container")
+
 frame_1.grid(row=1, column=0, padx=padx, pady=pady, sticky=tk.N)
 
 
 empty_entry_button = tk.Button(frame_1, text="Create", command=empty_entry)
-empty_entry_button.grid(row=2, column=0, padx=padx, pady=pady)
+empty_entry_button.grid(row=3, column=0, padx=padx, pady=pady)
 
 
 label_1 = tk.Label(frame_1, text="id")
-label_1.grid(row=0, column=0, padx=padx, pady=pady)
+label_1.grid(row=1, column=0, padx=padx, pady=pady)
 
 
 entry_1 = tk.Entry(frame_1, width=24, justify="right")
-entry_1.grid(row=1, column=0, padx=padx, pady=pady)
+entry_1.grid(row=2, column=0, padx=padx, pady=pady)
 entry_1.insert(0, "")
 
 empty_entry_button = tk.Button(frame_1, text="Update", command=empty_entry)
-empty_entry_button.grid(row=2, column=1, padx=padx, pady=pady)
+empty_entry_button.grid(row=3, column=1, padx=padx, pady=pady)
 
 
 label_1 = tk.Label(frame_1, text="Weight")
-label_1.grid(row=0, column=1, padx=padx, pady=pady)
+label_1.grid(row=1, column=1, padx=padx, pady=pady)
 
 
 entry_1 = tk.Entry(frame_1, width=24, justify="right")
-entry_1.grid(row=1, column=1, padx=padx, pady=pady)
+entry_1.grid(row=2, column=1, padx=padx, pady=pady)
 entry_1.insert(0, "")
 
 empty_entry_button = tk.Button(frame_1, text="Delete", command=empty_entry)
-empty_entry_button.grid(row=2, column=2, padx=padx, pady=pady)
+empty_entry_button.grid(row=3, column=2, padx=padx, pady=pady)
 
 
 label_1 = tk.Label(frame_1, text="Destination")
-label_1.grid(row=0, column=2, padx=padx, pady=pady)
+label_1.grid(row=1, column=2, padx=padx, pady=pady)
 
 
 entry_1 = tk.Entry(frame_1, width=24, justify="right")
-entry_1.grid(row=1, column=2, padx=padx, pady=pady)
+entry_1.grid(row=2, column=2, padx=padx, pady=pady)
 entry_1.insert(0, "")
 
 empty_entry_button = tk.Button(frame_1, text="Clear Entry Boxes", command=empty_entry)
-empty_entry_button.grid(row=2, column=3, padx=padx, pady=pady)
+empty_entry_button.grid(row=3, column=3, padx=padx, pady=pady)
 
 
 label_1 = tk.Label(frame_1, text="Weather")
-label_1.grid(row=0, column=3, padx=padx, pady=pady)
+label_1.grid(row=1, column=3, padx=padx, pady=pady)
 
 
 entry_1 = tk.Entry(frame_1, width=24, justify="right")
-entry_1.grid(row=1, column=3, padx=padx, pady=pady)
+entry_1.grid(row=2, column=3, padx=padx, pady=pady)
 entry_1.insert(0, "")
 
 read_table(tree_1)
